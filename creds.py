735f3fe251c4a2d01f270a21de565184e6cc1fb0
jacobajit <jacobvajit@gmail.com>
2016-03-23 06:18:42 -0400

    updated creds

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 #Alexa
-Security_Profile_Description ="Test profile"
-Security_Profile_ID  = "amzn1.application.7aa06fb8df2b41b684704045ad86ad1f"
-Client_ID = "amzn1.application-oa2-client.89aabb718c964b91b02d855967ec1a99"
-Client_Secret = "b282091f1c26d2081a9401b01c7d1046ebaf15185b271713e0e84afdf7e33295"
+Security_Profile_Description ="helloalexa"
+Security_Profile_ID  = "amzn1.application.c91c20c02ab846e3a6c24e0560281906"
+Client_ID = "amzn1.application-oa2-client.e962a1a2ecc143f49d53ec7cbda9fb54"
+Client_Secret = "2a9c44cb0fce54d84f8aac09e0e1969cd75be10de9005a1c9b7171cf484ff810"
 
 #Redis
 redis_url = "redis://h:pa40ceql93ak6r96ltf7umu86d2@ec2-107-20-158-183.compute-1.amazonaws.com:11389"
