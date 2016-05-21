07172d99939c152767d11619aaf636b6e786c257
jacobajit <jacobvajit@gmail.com>
2016-05-20 23:06:42 -0400

    f

--- app.py
+++ app.py
@@ -144,9 +144,9 @@ class AudioHandler(BaseHandler):
             self.set_status(403)
         else:
             print("geting argument...")
-            phrase=self.get_argument("text", default=None, strip=False)
-            print(phrase)
-            # phrase = "What is 22 divided by 2?"
+            # phrase=self.get_argument("text", default=None, strip=False)
+            # print(phrase)
+            phrase = "What is 22 divided by 2?"
             audio = requests.get('https://api.voicerss.org/', params={'key': '970f71e61a4b4c8abd6af0d1f6a5326e', 'src': phrase, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
             rxfile = audio.content
             #Response(audio.content, mimetype='audio/mpeg')
