e10d9d1d1cbde1944a45c879c7bbebccfc3f256e
jacobajit <jacobvajit@gmail.com>
2016-05-26 13:27:11 -0400

    f

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def getAlexa(text):
         print("getting post...")#
         # uid = tornado.escape.xhtml_escape(self.current_user)
         # token = gettoken(uid)
-        token="Atzr|IQEBLzAtAhUAgN-A_RZ2VuraDTEMShGiDzWxQrECFFC4j8XFI6U_pSrJqBpOUqdGNlncFu2ZVEILkHsVPb7tKU1k7vodb0NnTLlvz5FXr9rm0Jk-IuTjTF7S0rLBE1ElFXaD8FcaQfAr_rm7uGvTiRasn7i0HcENy8he249_-Jj2dL5-dGbNyyY-bQmp_5xAwv8UzCD8mIyDM9Z3BZSd1IgJr8W-qD30u5Pq2AIEpM7r_qDjYBPU_GfSOTIPdVNXqmswJ2AP4tte1_FMmnwwmhzSSMEXd_ankhhQ69zCCNQQQlMcmYIfe3rh2PaHnjO3Vsu63fpDeMUgDdnj91m7xaxsGO_SxP5hRSULb-vLLrAKNdweluxup6BnQg5MfGbikq5j1r5nSEkO0FhpDpkNa5mHXfVBUIlFX3kAqvSing-69gm5xl9UFT3KkIde6EtO9637Qi1UJXqi4doIzC8OdwuT9s6hA9FiOJL-4ORbUT3rkx9NYm68rqCwWmu27c597WzlMkGiRZ3RNuxg_58VbUFFZTpkr6rrqE51S0uUplSsn2N1"
+        token="Atza|IQEBLjAsAhQ1O1JJR7pESqfabkT4fkzeesVkcQIUT7DfZ8WPTYOB9wCT0TtFqaIAA564oqeYwQkk2UtgcfxSyxZ8pX5i2nITxzWCOYCXVglppxgXlvWqR1Ub84UYnNE_5KEHIqD0Fl3J69UoAGLN6pMG1i8G5ZB4FESUtg4M5QAMqB_1hjvhdHOtGa97_gIJTMkD64YQRok8zAHfD0F99ZQfDHVsPQTMhE0hoqXyVLgsg5S96kC3jNVCD6uDQSkW3n5uqD7c-jcCjFD9hBuSZmMz0YUUReulqeEZRUi0HxTeDR6gRSmQ39SNQ3xygUlKuDlw-wC9tGSpfnSeGfAMgnSirsiI4ECESWFqL_ez1ymxyHSNtx8zzN_JKCp58hd0kB0dt-Wk4eXTRg-HLu5JBv7-auuXRIx_M4QM5SLFlmISn9z168JMorMaeuQRpyFmIUj2Zve7b62ce2M1z-18wD41wlD7n5XhqRCfPjFR821zDysknZr-0gyqbliXzomsoiD_GAyLg-UBzRhFEM6W_oBU9Q"
         if (token == False):
             self.set_status(403)
         else:
