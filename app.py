f2ecb1328d9970eb6f243bf597e44af7aeac2164
jacobajit <jacobvajit@gmail.com>
2016-05-26 21:12:58 -0400

    stripping punctuation and validating string

--- app.py
+++ app.py
@@ -46,87 +46,89 @@ def getAlexa(text,mid):
             print("geting argument...")
             phrase=text
             print(phrase)
-            # phrase = "What is 22 divided by 2?"
-            audio = requests.get('https://api.voicerss.org/', params={'key': '970f71e61a4b4c8abd6af0d1f6a5326e', 'src': phrase, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
-            rxfile = audio.content
-            #Response(audio.content, mimetype='audio/mpeg')
-            #print("audio.content:  ", audio.content)
-            #rxfile = self.request.files['data'][0]['body']
-            tf = tempfile.NamedTemporaryFile(suffix=".wav")
-            tf.write(rxfile)
-            _input = AudioSegment.from_wav(tf.name)
-            tf.close()
+            exclude = set(string.punctuation)
+            phrase = ''.join(ch for ch in s if ch not in exclude)
+            if (phrase!=""):
 
-            #print("TF:  ", tf)
-            #print("RX:  ", rxfile)
+                # phrase = "What is 22 divided by 2?"
+                audio = requests.get('https://api.voicerss.org/', params={'key': '970f71e61a4b4c8abd6af0d1f6a5326e', 'src': phrase, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
+                rxfile = audio.content
+                #Response(audio.content, mimetype='audio/mpeg')
+                #print("audio.content:  ", audio.content)
+                #rxfile = self.request.files['data'][0]['body']
+                tf = tempfile.NamedTemporaryFile(suffix=".wav")
+                tf.write(rxfile)
+                _input = AudioSegment.from_wav(tf.name)
+                tf.close()
 
-            tf = tempfile.NamedTemporaryFile(suffix=".wav")
-            output = _input.set_channels(1).set_frame_rate(16000)
-            f = output.export(tf.name, format="wav")
-            url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
-            headers = {'Authorization' : 'Bearer %s' % token}
-            d = {
-                "messageHeader": {
-                    "deviceContext": [
-                        {
-                            "name": "playbackState",
-                            "namespace": "AudioPlayer",
-                            "payload": {
-                                "streamId": "",
-                                "offsetInMilliseconds": "0",
-                                "playerActivity": "IDLE"
+                #print("TF:  ", tf)
+                #print("RX:  ", rxfile)
+
+                tf = tempfile.NamedTemporaryFile(suffix=".wav")
+                output = _input.set_channels(1).set_frame_rate(16000)
+                f = output.export(tf.name, format="wav")
+                url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
+                headers = {'Authorization' : 'Bearer %s' % token}
+                d = {
+                    "messageHeader": {
+                        "deviceContext": [
+                            {
+                                "name": "playbackState",
+                                "namespace": "AudioPlayer",
+                                "payload": {
+                                    "streamId": "",
+                                    "offsetInMilliseconds": "0",
+                                    "playerActivity": "IDLE"
+                                }
                             }
-                        }
-                    ]
-                },
-                "messageBody": {
-                    "profile": "alexa-close-talk",
-                    "locale": "en-us",
-                    "format": "audio/L16; rate=16000; channels=1"
+                        ]
+                    },
+                    "messageBody": {
+                        "profile": "alexa-close-talk",
+                        "locale": "en-us",
+                        "format": "audio/L16; rate=16000; channels=1"
+                    }
                 }
-            }
-            files = [
-                ('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
-                ('file', ('audio', tf, 'audio/L16; rate=16000; channels=1'))
-            ]   
-            r = requests.post(url, headers=headers, files=files)
-            tf.close()
-            for v in r.headers['content-type'].split(";"):
-                if re.match('.*boundary.*', v):
-                    boundary =  v.split("=")[1]
-            data = r.content.split(boundary)
-            for d in data:
-                if (len(d) >= 1024):
-                   audio = d.split('\r\n\r\n')[1].rstrip('--')
+                files = [
+                    ('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
+                    ('file', ('audio', tf, 'audio/L16; rate=16000; channels=1'))
+                ]   
+                r = requests.post(url, headers=headers, files=files)
+                tf.close()
+                for v in r.headers['content-type'].split(";"):
+                    if re.match('.*boundary.*', v):
+                        boundary =  v.split("=")[1]
+                data = r.content.split(boundary)
+                for d in data:
+                    if (len(d) >= 1024):
+                       audio = d.split('\r\n\r\n')[1].rstrip('--')
 
 
-            tf2 = tempfile.NamedTemporaryFile(suffix=".mp3")
-            tf2.write(audio)
-            _input2 = AudioSegment.from_mp3(tf2.name)
-            tf2.close()
+                tf2 = tempfile.NamedTemporaryFile(suffix=".mp3")
+                tf2.write(audio)
+                _input2 = AudioSegment.from_mp3(tf2.name)
+                tf2.close()
 
-            tf3 = tempfile.NamedTemporaryFile(suffix=".wav")
-            output2=_input2.export(tf3.name, format="wav")
-
-
-            r = sr.Recognizer()
-            with sr.AudioFile(tf3) as source:
-                audio2 = r.record(source) # read the entire audio file
-
-            # recognize speech using Wit.ai
-            print(token)
-            WIT_AI_KEY = "ACGKCNOEUUXXHU3Q2SOMVCZW3MQMYUNW" # Wit.ai keys are 32-character uppercase alphanumeric strings
-            try:
-                transcription=r.recognize_wit(audio2, key=WIT_AI_KEY)
-                print("Wit.ai thinks you said " + transcription )
-            except sr.UnknownValueError:
-                print("Wit.ai could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Wit.ai service; {0}".format(e))
-
-             
+                tf3 = tempfile.NamedTemporaryFile(suffix=".wav")
+                output2=_input2.export(tf3.name, format="wav")
 
 
+                r = sr.Recognizer()
+                with sr.AudioFile(tf3) as source:
+                    audio2 = r.record(source) # read the entire audio file
+
+                # recognize speech using Wit.ai
+                print(token)
+                WIT_AI_KEY = "ACGKCNOEUUXXHU3Q2SOMVCZW3MQMYUNW" # Wit.ai keys are 32-character uppercase alphanumeric strings
+                try:
+                    transcription=r.recognize_wit(audio2, key=WIT_AI_KEY)
+                    print("Wit.ai thinks you said " + transcription )
+                except sr.UnknownValueError:
+                    print("Wit.ai could not understand audio")
+                except sr.RequestError as e:
+                    print("Could not request results from Wit.ai service; {0}".format(e))
+
+                 
 
 
 
@@ -134,7 +136,11 @@ def getAlexa(text,mid):
 
 
 
-            return transcription
+
+
+                return transcription
+            else:
+                return "Sorry, we couldn't understand your message."
     
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -375,6 +381,7 @@ class AudioHandler(BaseHandler):
             print("geting argument...")
             phrase=self.get_argument("text", default=None, strip=False)
             print(phrase)
+
             # phrase = "What is 22 divided by 2?"
             audio = requests.get('http://www.voicerss.org/controls/speech.ashx', params={'src': phrase, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
             rxfile = audio.content
