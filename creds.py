c5b3eaf617410dd1e4ac50ced051c33e75f834f8
jacobajit <jacobvajit@gmail.com>
2016-05-27 18:54:36 -0400

    moved config vars to heroku

--- creds.py
+++ creds.py
@@ -1,14 +1,14 @@
 #Alexa
-Security_Profile_Description ="helloalexa"
-Security_Profile_ID  = "amzn1.application.c91c20c02ab846e3a6c24e0560281906"
-Client_ID = "amzn1.application-oa2-client.e962a1a2ecc143f49d53ec7cbda9fb54"
-Client_Secret = "2a9c44cb0fce54d84f8aac09e0e1969cd75be10de9005a1c9b7171cf484ff810"
+Security_Profile_Description = os.environ['SECURITY_PROFILE_DESCRIPTION']
+Security_Profile_ID  = os.environ['SECURITY_PROFILE_ID']
+Client_ID = os.environ['CLIENT_ID']
+Client_Secret = os.environ['CLIENT_SECRET']
 
 #Facebook
-Facebook_Token="EAAJPHXpVWQIBAMXZC5aDFfQuCUUQfHIm65ccxiN39ymIz7haOVIpxFVqWsP5QviRZAc4xMdCvReTl2nzSikctr9ZAskZAABxzNpWZCCAeosR1mmsbqZBkpoZAbQNj9qdwD3PbxGowNNvGrTmqZBqFH5i6uSCF6aSDc0kaU1rM6RZCDAZDZD"
+Facebook_Token= os.environ['FACEBOOK_TOKEN']
 
 #wit.ai
-Wit_Token= "ACGKCNOEUUXXHU3Q2SOMVCZW3MQMYUNW"
+Wit_Token= os.environ['WIT_TOKEN']
 
 #Redis
-redis_url = "redis://h:p6laudn0u2s68r8lbudviqqhc67@ec2-107-20-158-183.compute-1.amazonaws.com:11389"
+redis_url = os.environ['REDIS_URL']
