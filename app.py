bf8a4070542a2e9de845ebf0adcbba853cf40212
Haicheng Charles Zhao <czhao39@gmail.com>
2016-05-31 19:59:23 -0400

    Structured message debugging

--- app.py
+++ app.py
@@ -233,10 +233,8 @@ class MessageHandler(BaseHandler):
                 if payload=="AUTH":
                     print("Generating login link...")
                     link='https://helloalexa.herokuapp.com/start?mid='+recipient_id
-                    recipient = {"id": recipient_id}
                     messageData = {"attachment": {"type": "template","payload": {"template_type": "generic","elements": [{"title": "Login to Amazon","buttons": [{"type": "web_url","url": link,"title": "Login"}]}]}}}
-                    payload = {"recipient": recipient, "message": messageData}
-                    print("recipient:", recipient)
+                    payload = {"recipient": {"id": recipient_id}, "message": messageData}
                     r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+TOKEN, json=payload)
                     print(r.text)
                     print("Made post request")
