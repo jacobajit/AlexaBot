991d5f77dbf2d5bcff97ded709829f19fabad755
jacobajit <jacobvajit@gmail.com>
2016-06-05 16:16:48 -0400

    debugging

--- app.py
+++ app.py
@@ -134,16 +134,16 @@ def getAlexa(text,mid):
                 print("Could not request results from Wit.ai service; {0}".format(e))
 
 
-            # recognize speech using Google Speech Recognition
-            try:
-                # for testing purposes, we're just using the default API key
-                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                # instead of `r.recognize_google(audio)`
-                print("Google Speech Recognition thinks you said " + r.recognize_google(audio2))
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            # # recognize speech using Google Speech Recognition
+            # try:
+            #     # for testing purposes, we're just using the default API key
+            #     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            #     # instead of `r.recognize_google(audio)`
+            #     print("Google Speech Recognition thinks you said " + r.recognize_google(audio2))
+            # except sr.UnknownValueError:
+            #     print("Google Speech Recognition could not understand audio")
+            # except sr.RequestError as e:
+            #     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
             return transcription
