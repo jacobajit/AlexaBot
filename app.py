1a4f1e29b1c3ff3da7cb91e456d974ae53bb2c10
jacobajit <jacobvajit@gmail.com>
2016-06-07 16:11:22 -0400

    debugging

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def getAlexa(text,mid):
                 # for testing purposes, we're just using the default API key
                 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                 # instead of `r.recognize_google(audio)`
-                print("Google Speech Recognition thinks you said " + r.recognize_google(audio2, key="AIzaSyDtC4Lu1u2MV6FNEk7ZJOcoLrMa9bOnUlE"))
+                print("Google Speech Recognition thinks you said " + recognize_google(audio2, key="AIzaSyDtC4Lu1u2MV6FNEk7ZJOcoLrMa9bOnUlE"))
             except sr.UnknownValueError:
                 print("Google Speech Recognition could not understand audio")
             except sr.RequestError as e:
