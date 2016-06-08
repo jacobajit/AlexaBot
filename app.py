c50a5c687ade5a8a4950969ca0c38d1f99635a66
jacobajit <jacobvajit@gmail.com>
2016-06-07 20:30:58 -0400

    debugging

--- app.py
+++ app.py
@@ -978,8 +978,8 @@ def getAlexa(text,mid):
             print(token)
             WIT_AI_KEY = Wit_Token # Wit.ai keys are 32-character uppercase alphanumeric strings
             try:
-                transcription=r.recognize_wit(audio2, key=WIT_AI_KEY)
-                print("Wit.ai thinks you said " + transcription )
+                transcriptionW=r.recognize_wit(audio2, key=WIT_AI_KEY)
+                print("Wit.ai thinks you said " + transcriptionW )
             except UnknownValueError:
                 print("Wit.ai could not understand audio")
             except RequestError as e:
@@ -991,14 +991,15 @@ def getAlexa(text,mid):
                 # for testing purposes, we're just using the default API key
                 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                 # instead of `r.recognize_google(audio)`
-                print("Google Speech Recognition thinks you said " + r.recognize_google(audio2, key="AIzaSyDtC4Lu1u2MV6FNEk7ZJOcoLrMa9bOnUlE"))
+                transcriptionG=r.recognize_google(audio2, key="AIzaSyDtC4Lu1u2MV6FNEk7ZJOcoLrMa9bOnUlE")
+                print("Google Speech Recognition thinks you said " + transcriptionG)
             except UnknownValueError:
                 print("Google Speech Recognition could not understand audio")
             except RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-            return transcription
+            return transcriptionG
 
     
 class BaseHandler(tornado.web.RequestHandler):
