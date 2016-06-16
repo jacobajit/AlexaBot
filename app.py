87e8eb510205b6262fc224aa0cccb96ac4cadfba
Haicheng Charles Zhao <czhao39@gmail.com>
2016-06-16 15:27:15 -0400

    remove unnecessary wit call

--- app.py
+++ app.py
@@ -629,34 +629,34 @@ def getAlexa(text,mid):
             with AudioFile(tf3) as source:
                 audio2 = r.record(source) # read the entire audio file
 
-            # recognize speech using Wit.ai
-            print(token)
-            WIT_AI_KEY = Wit_Token # Wit.ai keys are 32-character uppercase alphanumeric strings
-            try:
-                transcriptionW=r.recognize_wit(audio2, key=WIT_AI_KEY)
-                print("Wit.ai thinks you said " + transcriptionW )
-            except UnknownValueError:
-                print("Wit.ai could not understand audio")
-            except RequestError as e:
-                print("Could not request results from Wit.ai service; {0}".format(e))
-
+           
 
             # recognize speech using Google Speech Recognition
             try:
                 # for testing purposes, we're just using the default API key
                 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                 # instead of `r.recognize_google(audio)`
-                transcriptionG=r.recognize_google(audio2, key=Google_Speech_Token)
+                transcription=r.recognize_google(audio2, key=Google_Speech_Token)
                 print("Google Speech Recognition thinks you said " + transcriptionG)
-            except UnknownValueError:
-                transcriptionG=transcriptionW
+            except (UnknownValueError, RequestError):
+                 # recognize speech using Wit.ai
+                print(token)
+                WIT_AI_KEY = Wit_Token # Wit.ai keys are 32-character uppercase alphanumeric strings
+                try:
+                    transcription=r.recognize_wit(audio2, key=WIT_AI_KEY)
+                    print("Wit.ai thinks you said " + transcriptionW )
+                except UnknownValueError:
+                    print("Wit.ai could not understand audio")
+                except RequestError as e:
+                    print("Could not request results from Wit.ai service; {0}".format(e))
+
                 print("Google Speech Recognition could not understand audio")
-            except RequestError as e:
-                transcriptionG=transcriptionW
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            #except RequestError as e:
+            #    transcriptionG=transcriptionW
+            #    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-            return transcriptionG
+            return transcription
 
     
 class BaseHandler(tornado.web.RequestHandler):
