b71d0ca8aade3b822566ec1c1d4317fa414a939f
Jacob Ajit <jacobvajit@gmail.com>
2019-02-09 04:46:12 -0500

    Cleanup and comments

--- app.py
+++ app.py
@@ -12,19 +12,16 @@ import tempfile
 import redis
 import uuid
 import string
-from pydub import AudioSegment
-# import speech_recognition as sr
 from pymessenger.bot import Bot
 import traceback
 
+# PyDub has some issues with Google Speech API params - fixed in pyDubMod
 from pyDubMod import *
 
-from timeout_dec import timeout_dec  # timeout decorator
+# Timeout decorator
+from timeout_dec import timeout_dec
 
-
-TOKEN= Facebook_Token
-bot = Bot(TOKEN)
-
+bot = Bot(Facebook_Token)
 
 def shutil_which(pgm):
     """Python 2 backport of ``shutil.which()`` from Python 3"""
@@ -34,7 +31,6 @@ def shutil_which(pgm):
         if os.path.exists(p) and os.access(p, os.X_OK):
             return p
 
-    
 def gettoken(uid):
     red = redis.from_url(redis_url)
     token = red.get(uid+"-access_token")
@@ -57,117 +53,116 @@ def gettoken(uid):
     else:
         return False
 
-#function version of getting Alexa's response in text
+# Get Alexa's [text] response to a [text] query
 @timeout_dec(20)
-def getAlexa(text,mid):
-        print("getting post...")#
-        # uid = tornado.escape.xhtml_escape(self.current_user)
-        token = gettoken(mid)
-        #token=""
-        if (token == False):
-            red = redis.from_url(redis_url)
-            red.delete(mid+"-refresh_token")
-            return "Sorry, it looks like you didn't log in to Amazon correctly. Try again here https://amazonalexabot.herokuapp.com/start and come back with your code."
-        else:
-            print("geting argument...")
-            phrase=text
-            print(phrase)
-            #http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=hello&tl=En-us
-            audio = requests.get('https://api.voicerss.org/', params={'key': VoiceRSS_Token, 'src': phrase, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
-            rxfile = audio.content
+def getAlexa(text, mid):
+    # Fetch user's Amazon access token with Messenger ID as the uid
+    token = gettoken(mid)
 
-            tf = tempfile.NamedTemporaryFile(suffix=".wav")
-            tf.write(rxfile)
-            _input = AudioSegment.from_wav(tf.name)
-            tf.close()
+    # Nonexistent or broken refresh token - was more of an issue in older version, when user manually entered refresh token
+    if (not token):
+        # Remove any broken refresh token
+        red = redis.from_url(redis_url)
+        red.delete(mid + "-refresh_token")
+        return "Sorry, it looks like you didn't log in to Amazon correctly. Try again here https://amazonalexabot.herokuapp.com/start and come back with your code."
 
-            tf = tempfile.NamedTemporaryFile(suffix=".wav")
-            output = _input.set_channels(1).set_frame_rate(16000)
-            f = output.export(tf.name, format="wav")
-            url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
-            headers = {'Authorization' : 'Bearer %s' % token}
-            d = {
-                "messageHeader": {
-                    "deviceContext": [
-                        {
-                            "name": "playbackState",
-                            "namespace": "AudioPlayer",
-                            "payload": {
-                                "streamId": "",
-                                "offsetInMilliseconds": "0",
-                                "playerActivity": "IDLE"
-                            }
-                        }
-                    ]
-                },
-                "messageBody": {
-                    "profile": "alexa-close-talk",
-                    "locale": "en-us",
-                    "format": "audio/L16; rate=16000; channels=1"
+    # Google Translate TTS was also considered -
+    # http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=hello&tl=En-us
+
+    # Speech synthesis through VoiceRSS API
+    audio = requests.get('https://api.voicerss.org/', params={'key': VoiceRSS_Token, 'src': text, 'hl': 'en-us', 'c': 'WAV', 'f': '16khz_16bit_mono'})
+
+    # Write out synthesized speech to a temporary file
+    tf = tempfile.NamedTemporaryFile(suffix=".wav")
+    tf.write(audio.content)
+
+    # Create an AudioSegment object from synthesized audio file
+    _input = AudioSegment.from_wav(tf.name)
+    tf.close()
+
+    # Convert audio object - mono channel 16 khz for Alexa Voice Service
+    _output = _input.set_channels(1).set_frame_rate(16000)
+
+    # Formatted synthesized audio file
+    audio_infile = _output.export(format="wav")
+
+    # Parameters for AVS request
+    url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
+    headers = {'Authorization' : 'Bearer %s' % token}
+    avs_json = {
+        "messageHeader": {
+            "deviceContext": [
+                {
+                    "name": "playbackState",
+                    "namespace": "AudioPlayer",
+                    "payload": {
+                        "streamId": "",
+                        "offsetInMilliseconds": "0",
+                        "playerActivity": "IDLE"
+                    }
                 }
-            }
-            files = [
-                ('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
-                ('file', ('audio', tf, 'audio/L16; rate=16000; channels=1'))
-            ]   
-            r = requests.post(url, headers=headers, files=files)
-            tf.close()
-            for v in r.headers['content-type'].split(";"):
-                if re.match('.*boundary.*', v):
-                    boundary =  v.split("=")[1]
+            ]
+        },
+        "messageBody": {
+            "profile": "alexa-close-talk",
+            "locale": "en-us",
+            "format": "audio/L16; rate=16000; channels=1"
+        }
+    }
+    files = [
+        ('file', ('request', json.dumps(avs_json), 'application/json; charset=UTF-8')),
+        ('file', ('audio', audio_infile, 'audio/L16; rate=16000; channels=1'))
+    ]   
 
-            data = r.content.split(boundary)
-            for d in data:
-                if (len(d) >= 1024):
-                   audio = d.split('\r\n\r\n')[1].rstrip('--')
+    # Make request to AVS
+    r = requests.post(url, headers=headers, files=files)
 
-            tf2 = tempfile.NamedTemporaryFile(suffix=".mp3")
-            tf2.write(audio)
-            _input2 = AudioSegment.from_mp3(tf2.name)
-            tf2.close()
+    for v in r.headers['content-type'].split(";"):
+        if re.match('.*boundary.*', v):
+            boundary =  v.split("=")[1]
 
-            #convert mp3 file to wav
-            tf3 = tempfile.NamedTemporaryFile(suffix=".wav")
-            #output2=_input2.export(tf3.name, format="wav",bitrate="16k",parameters=["-ac", "1", "-acodec", "pcm_s16le"])
-            output2=_input2.export(tf3.name, format="wav")
- 
-            r = Recognizer()
-            with AudioFile(tf3) as source:
-                audio2 = r.record(source) # read the entire audio file
+    data = r.content.split(boundary)
+    for d in data:
+        if (len(d) >= 1024):
+           audio_outfile = d.split('\r\n\r\n')[1].rstrip('--')
 
-           # # recognize speech using Microsoft Bing Voice Recognition
-           #  BING_KEY = "578545f1fb3940fb99151cfd79b476b1" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-           #  try:
-           #      print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio2, key=BING_KEY))
-           #  except UnknownValueError:
-           #      print("Microsoft Bing Voice Recognition could not understand audio")
-           #  except RequestError as e:
-           #      print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+    # Temporary file to store Alexa audio output
+    tf = tempfile.NamedTemporaryFile(suffix=".mp3")
+    tf.write(audio_outfile)
 
-            # recognize speech using Google Speech Recognition
-            try:
-                # for testing purposes, we're just using the default API key
-                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                # instead of `r.recognize_google(audio)`
-                transcription=r.recognize_google(audio2, key=Google_Speech_Token)
-                print("Google Speech Recognition thinks you said " + transcription)
-            except (UnknownValueError, RequestError):
-                 # recognize speech using Wit.ai
-                print(token)
-                WIT_AI_KEY = Wit_Token # Wit.ai keys are 32-character uppercase alphanumeric strings
-                try:
-                    transcription=r.recognize_wit(audio2, key=WIT_AI_KEY)
-                    print("Wit.ai thinks you said " + transcription)
-                except UnknownValueError:
-                    print("Wit.ai could not understand audio")
-                except RequestError as e:
-                    print("Could not request results from Wit.ai service; {0}".format(e))
+    # Create AudioSegment object for Alexa audio output
+    _input = AudioSegment.from_mp3(tf.name)
+    tf.close()
 
-                print("Google Speech Recognition could not understand audio")
+    # Export Alexa audio output in wav format
+    wav_audio_outfile =_input.export(tf3.name, format="wav")
 
-            return transcription
+    # Speech recognizer object initialization
+    r = Recognizer()
+    with AudioFile(wav_audio_outfile) as source:
+        audio = r.record(source) # read the entire audio file
 
-    
+    # Recognize speech using Google Speech Recognition
+    try:
+        transcription = r.recognize_google(audio, key=Google_Speech_Token)
+
+    # Fallback speech recognition
+    except (UnknownValueError, RequestError):
+        print("Google Speech Recognition could not understand audio")
+
+         # Recognize speech using Wit.ai
+        WIT_AI_KEY = Wit_Token # Wit.ai keys are 32-character uppercase alphanumeric strings
+        try:
+            transcription = r.recognize_wit(audio, key=WIT_AI_KEY)
+            print("Wit.ai thinks you said " + transcription)
+        except UnknownValueError:
+            print("Wit.ai could not understand audio")
+        except RequestError as e:
+            print("Could not request results from Wit.ai service; {0}".format(e))
+
+    return transcription
+
+
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return self.get_cookie("user")
@@ -239,7 +234,10 @@ class LogoutHandler(BaseHandler):
         self.write("Logged Out, Goodbye")
         self.finish()
 
+# Facebook Messenger webhook
 class MessageHandler(BaseHandler):
+    
+    # Verify webhook
     @tornado.web.asynchronous
     def get(self):
         if (self.get_argument("hub.verify_token", default=None, strip=False) == "my_voice_is_my_password_verify_me"):
@@ -247,60 +245,75 @@ class MessageHandler(BaseHandler):
             self.write(self.get_argument("hub.challenge", default=None, strip=False))
             self.finish()
     
+    # Receive messages from users
     def post(self):
-        output = tornado.escape.json_decode(self.request.body) 
-        print("OUTPUT: ",output)
-        event = output['entry'][0]['messaging']
+        fb_json = tornado.escape.json_decode(self.request.body) 
+        event = fb_json['entry'][0]['messaging']
+        
         for x in event:
+            # User's messenger ID (MID)
             recipient_id = x['sender']['id']
+            
+            # Get Started button - used for AVS authentication
             if "postback" in x and "payload" in x['postback']:
                 payload = x['postback']['payload']
-                if payload=="AUTH":
-                    print("Generating login link...")
-                    link='https://amazonalexabot.herokuapp.com/start?mid='+recipient_id
+                
+                # User authentication for AVS
+                if payload == "AUTH":
+                    # Generate login link with user's MID
+                    link = "https://amazonalexabot.herokuapp.com/start?mid=" + recipient_id
+                    
+                    # Send a login dialog to user in Messenger
                     messageData = {"attachment": {"type": "template","payload": {"template_type": "generic","elements": [{"title": "Login to Amazon","buttons": [{"type": "web_url","url": link,"title": "Login"}]}]}}}
                     payload = {"recipient": {"id": recipient_id}, "message": messageData}
-                    r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+TOKEN, json=payload)
-                    print(r.text)
-                    print("Made post request")
+                    r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+Facebook_Token, json=payload)
+
+            # Received sticker
             elif "message" in x and "sticker_id" in x["message"]:
-                print("received sticker")
                 bot.send_text_message(recipient_id, "(y)")
-            elif "message" in x and "text" in x['message']:
-                message = x['message']['text']
-                print("The message:", message)
+           
+            # Received a textual message
+            elif "message" in x and "text" in x["message"]:
+                message = x["message"]["text"]
                 try:
+                    # Hardcode a few greetings that are problematic for the speech synthesis
                     if message.lower() in {"hi", "hello", "hi alexa", "hello alexa","hi there","hey alexa","hey", "hello there"}:
                         bot.send_text_message(recipient_id, "Hi there")
+                        
+                    # Help message required by Facebook
                     elif message.lower() in {"help", "help me"}:
                         bot.send_text_message(recipient_id, "Type anything you would say to Amazon's Alexa assistant and receive her response. For more help with what you can say, check out the Things to Try section of the Alexa app.")
+                    
+                    # Normal textual message
                     else:
                         red = redis.from_url(redis_url)
+                        
+                        # User is not/improperly logged into Amazon - used to handle direct text refresh tokens here
                         if not red.exists(recipient_id+"-refresh_token"):
-                            print("Received refresh token")
-                            red.set(recipient_id+"-refresh_token", message)
-                            testing=gettoken(recipient_id)
-                            if(testing==False):
-                                red.delete(recipient_id+"-refresh_token")
-                                link='https://amazonalexabot.herokuapp.com/start?mid='+recipient_id
-                                messageData = {"attachment": {"type": "template","payload": {"template_type": "generic","elements": [{"title": "You are not logged in properly.","buttons": [{"type": "web_url","url": link,"title": "Login"}]}]}}}
-                                payload = {"recipient": {"id": recipient_id}, "message": messageData}
-                                r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+TOKEN, json=payload)
-                            else:
-                                bot.send_text_message(recipient_id, "Great, you're logged in. Start talking to Alexa!")
-                          
+                            # Generate login link with user's MID
+                            link='https://amazonalexabot.herokuapp.com/start?mid='+recipient_id
+                            
+                            # Send login dialog to user in Messenger
+                            messageData = {"attachment": {"type": "template","payload": {"template_type": "generic","elements": [{"title": "You are not logged in properly.","buttons": [{"type": "web_url","url": link,"title": "Login"}]}]}}}
+                            payload = {"recipient": {"id": recipient_id}, "message": messageData}
+                            r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+Facebook_Token, json=payload)
+                        
+                        # User is logged into Amazon
                         else:
-                            print("Getting Alexa's response from AudioHandler. Message was: "+message)
-                            # alexaresponse = requests.get('https://amazonalexabot.herokuapp.com/audio', params={'text': message})
-                            alexaresponse = getAlexa(message,recipient_id)
-                            print("Alexa's response: ", alexaresponse)
-                            # bot.send_text_message(recipient_id, alexaresponse.text)
-                            if len(alexaresponse) > 320:
-                                alexaresponse = alexaresponse[:317] + "..."
-                            bot.send_text_message(recipient_id, alexaresponse)
+                            # Get response from Alexa - convert text-to-speech, pass through AVS, and then convert speech-to-text
+                            alexa_response = getAlexa(message, recipient_id)
+                            
+                            # Truncate response
+                            if len(alexa_response) > 320:
+                                alexa_response = alexaresponse[:317] + "..."
+                                
+                            # Send Alexa's textual response to Messenger user
+                            bot.send_text_message(recipient_id, alexa_response)
+                            
                 except TimeoutError:
                     print(traceback.format_exc())
                     bot.send_text_message(recipient_id, "Request took too long.")
+                    
                 except Exception,err:
                     print("Couldn't understand: ", traceback.format_exc())
                     bot.send_text_message(recipient_id, "Alexa gave an invalid response. This may occur if you gave Alexa a command such as \"Turn on the lights,\" which requires no reply from Alexa. Otherwise, something went wrong and we are trying to fix it!")
@@ -309,14 +322,9 @@ class MessageHandler(BaseHandler):
         self.set_status(200)
         self.finish()
 
-    def write_error(self, status_code, **kwargs):
-        self.write("Gosh darnit, user! You caused a %d error." % status_code)
-
-
 def main():
     settings = {
         "cookie_secret": "parisPOLANDbroadFENCEcornWOULD",
-        # url
     }
     static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
     application = tornado.web.Application([(r"/", MainHandler),
